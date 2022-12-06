e92368523ea03b108ac56bbb7ed718e17ca02bd6
Nathan McCrina <nathan.mccrina@outlook.com>
2022-12-06 08:48:53 -0600

    rust day six part one

---
+++ 2022/rust/day_six/src/main.rs
@@ -0,0 +1,60 @@
+use std::fs;
+
+fn get_data() -> String {
+    let input = match fs::read_to_string("input") {
+        Ok(data) => data,
+        Err(error) => panic!("Error opening input file: {:?}", error),
+    };
+
+    input
+}
+
+fn is_match(current_group: u32) -> bool {
+    let bytes: (u8, u8, u8, u8) = (
+        ((current_group & 0xFF000000) >> 24) as u8,
+        ((current_group & 0x00FF0000) >> 16) as u8,
+        ((current_group & 0x0000FF00) >> 8) as u8,
+        (current_group & 0x000000FF) as u8,
+    );
+
+    bytes.0 == bytes.1
+        || bytes.0 == bytes.2
+        || bytes.0 == bytes.3
+        || bytes.1 == bytes.0
+        || bytes.1 == bytes.2
+        || bytes.1 == bytes.3
+        || bytes.2 == bytes.0
+        || bytes.2 == bytes.1
+        || bytes.2 == bytes.3
+        || bytes.3 == bytes.0
+        || bytes.3 == bytes.1
+        || bytes.3 == bytes.2
+}
+
+fn part_one() {
+    let input = get_data();
+    let input_bytes = input.into_bytes();
+    let mut current_group: u32 = 0;
+    let mut index = 0;
+    let mut next_byte: u8 = input_bytes[index];
+
+    while index < 4 {
+        current_group <<= 8;
+        current_group |= next_byte as u32;
+        index += 1;
+        next_byte = input_bytes[index];
+    }
+
+    while is_match(current_group) {
+        current_group <<= 8;
+        current_group |= next_byte as u32;
+        index += 1;
+        next_byte = input_bytes[index];
+    }
+
+    println!("Marker appears after position {}", index);
+}
+
+fn main() {
+    part_one();
+}
